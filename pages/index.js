deeddb035cc774240354cd55b428cf2dc7ae4a4d
navidabdi <artsanaat@gmail.com>
2022-02-13 18:39:20 +0330

    change the name

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
-import Header from '../Components/Header'
-import Nav from '../Components/Nav'
-import Results from '../Components/Results'
+import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Results from '../components/Results'
 import requests from '../utils/requests'
 
 export default function Home({ results }) {
   return (
     <div className="m-auto w-[93%]">
       <Head>
-        <title>TMDB 1.0</title>
+        <title>TMDB 1.0.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
